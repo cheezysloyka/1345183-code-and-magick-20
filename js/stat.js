783e74fbeb9ff5de3095e2505d0c2b47035dba9c
cheezy_sloyka <vishnevskaya97@gmail.com>
2020-06-07 08:06:52 +0300

    iqjdnvfiw

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,6 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var BAR_GAP = 50;
 var TEXT_HEIGHT = 20;
-var TEXT_WIDTH = 90;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 
@@ -18,7 +17,7 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -51,8 +50,7 @@ window.renderStatistics = function (ctx, players, times) {
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT, BAR_WIDTH, ((BAR_HEIGHT * times[i]) / maxTime) * (-1));
-    }
-    else {
+    } else {
       var rand = Math.round(Math.random() * 100);
       ctx.fillStyle = 'hsl(240, ' + rand + '%, 20%)';
       ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT, BAR_WIDTH, ((BAR_HEIGHT * times[i]) / maxTime) * (-1));
