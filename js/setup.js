76d7c13c282c2c11074740585ca303153370cda2
cheezy_sloyka <vishnevskaya97@gmail.com>
2020-06-16 08:28:37 +0300

    поправила константы

--- js/setup.js
+++ js/setup.js
@@ -7,14 +7,14 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var WIZARD_NAMES = [];
-var RANDOM_NAME;
-var WIZARD_SURNAMES = [];
-var RANDOM_SURNAME;
-var COAT_COLOR = [];
-var RANDOM_COAT_COLOR;
-var EYES_COLOR = [];
-var RANDOM_EYES_COLOR;
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var randomName;
+var randomSurname;
+var randomCoat;
+var randomEyes;
 var wizards = [];
 var wizardElement;
 
@@ -28,38 +28,31 @@ showBlock();
 function renderWizard() {
 
   for (var i = 0; i < 4; i++) {
-    WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-    RANDOM_NAME = Math.floor(Math.random() * WIZARD_NAMES.length);
-
-    WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-    RANDOM_SURNAME = Math.floor(Math.random() * WIZARD_SURNAMES.length);
-
-    COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-    RANDOM_COAT_COLOR = Math.floor(Math.random() * COAT_COLOR.length);
-
-    EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-    RANDOM_EYES_COLOR = Math.floor(Math.random() * EYES_COLOR.length);
+    randomName = Math.floor(Math.random() * WIZARD_NAMES.length);
+    randomSurname = Math.floor(Math.random() * WIZARD_SURNAMES.length);
+    randomCoat = Math.floor(Math.random() * COAT_COLOR.length);
+    randomEyes = Math.floor(Math.random() * EYES_COLOR.length);
 
     wizards = [
       {
-        name: WIZARD_NAMES[RANDOM_NAME] + ' ' + WIZARD_SURNAMES[RANDOM_SURNAME],
-        coatColor: COAT_COLOR[RANDOM_COAT_COLOR],
-        eyesColor: EYES_COLOR[RANDOM_EYES_COLOR]
+        name: WIZARD_NAMES[randomName] + ' ' + WIZARD_SURNAMES[randomSurname],
+        coatColor: COAT_COLOR[randomCoat],
+        eyesColor: EYES_COLOR[randomEyes]
       },
       {
-        name: WIZARD_NAMES[RANDOM_NAME] + ' ' + WIZARD_SURNAMES[RANDOM_SURNAME],
-        coatColor: COAT_COLOR[RANDOM_COAT_COLOR],
-        eyesColor: EYES_COLOR[RANDOM_EYES_COLOR]
+        name: WIZARD_NAMES[randomName] + ' ' + WIZARD_SURNAMES[randomSurname],
+        coatColor: COAT_COLOR[randomCoat],
+        eyesColor: EYES_COLOR[randomEyes]
       },
       {
-        name: WIZARD_NAMES[RANDOM_NAME] + ' ' + WIZARD_SURNAMES[RANDOM_SURNAME],
-        coatColor: COAT_COLOR[RANDOM_COAT_COLOR],
-        eyesColor: EYES_COLOR[RANDOM_EYES_COLOR]
+        name: WIZARD_NAMES[randomName] + ' ' + WIZARD_SURNAMES[randomSurname],
+        coatColor: COAT_COLOR[randomCoat],
+        eyesColor: EYES_COLOR[randomEyes]
       },
       {
-        name: WIZARD_NAMES[RANDOM_NAME] + ' ' + WIZARD_SURNAMES[RANDOM_SURNAME],
-        coatColor: COAT_COLOR[RANDOM_COAT_COLOR],
-        eyesColor: EYES_COLOR[RANDOM_EYES_COLOR]
+        name: WIZARD_NAMES[randomName] + ' ' + WIZARD_SURNAMES[randomSurname],
+        coatColor: COAT_COLOR[randomCoat],
+        eyesColor: EYES_COLOR[randomEyes]
       },
     ];
 
